642d43947cd86b0b1979d9abc60d914e4790821d
wanidap <57169424+wanidap@users.noreply.github.com>
2022-05-24 13:14:14 +0700

    Update reApp.py

--- reApp.py
+++ reApp.py
@@ -41,13 +41,13 @@ sacs in one or both lungs. There are 5,856 x-ray images out of which 4,273 are p
     st.write("For the Pneumonia recognition dataset, we resized all images to an appropriate size based on the CNN model, and every image had been converted from RGB to grayscale color image. Moreover, feature extraction is a powerful technology that influences image recognition accuracy. We have used an automatic extraction algorithm called Convolutional Neural Networks (CNN) for feature extraction.")
     st.write("The training and testing accuracy for Pneumonia recognition dataset are 94.96% and 94.11%, respectively which is given by DenseNet201 CNN combined with our SGD-GSVM model.")
     with st.sidebar:
+        img = Image.open("logo1.png")
+        st.image(img, width=300)
         st.header("Contact:")
         st.write("Wanida Panup")
         st.write("Tel. 088-4365416")
         st.write("Email. wanidap56@nu.ac.th")
         st.write("Address: 54/1, Moo 4, Khun Fang subdistrict, Mueang Uttaradit, Uttaradit Province, Thailand 53000")
-        img = Image.open("logo1.png")
-        st.image(img, width=300)
         img = Image.open("NUlogo.png")
         st.image(img, width=100)
 def get_feature_(img):
